5ecfee1fd569760d4c071d5797c281fb4ebd048e
Cipran Fodor <cyp892@yahoo.com>
2024-07-31 04:58:22 +0300

    sall

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,12 +1,12 @@
-import path from "path";
-import express from "express";
-import multer from "multer";
+import path from 'path';
+import express from 'express';
+import multer from 'multer';
 
 const router = express.Router();
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, 'uploads/');
   },
   filename(req, file, cb) {
     cb(
@@ -18,26 +18,29 @@ const storage = multer.diskStorage({
 
 function fileFilter(req, file, cb) {
   const filetypes = /jpe?g|png|webp/;
-  const mimetype = /image\/jpe?g|image\/png|image\/webp/;
+  const mimetypes = /image\/jpe?g|image\/png|image\/webp/;
+
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = mimetypes.test(file.mimetype);
 
   if (extname && mimetype) {
     cb(null, true);
   } else {
-    cb(new Error("Invalid file"), false);
+    cb(new Error('Images only!'), false);
   }
 }
 
 const upload = multer({ storage, fileFilter });
-const uploadSingleImage = upload.single("image");
+const uploadSingleImage = upload.single('image');
 
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
   uploadSingleImage(req, res, function (err) {
     if (err) {
-      res.status(400).send({ message: err.message });
+      return res.status(400).send({ message: err.message });
     }
+
     res.status(200).send({
-      message: "Uploading image successfully",
+      message: 'Image uploaded successfully',
       image: `/${req.file.path}`,
     });
   });
