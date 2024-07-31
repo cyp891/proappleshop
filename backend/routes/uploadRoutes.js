746a9e82650078d52864d4bb3f4679164807e48a
Cipran Fodor <cyp892@yahoo.com>
2024-07-31 23:44:52 +0300

    yes

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,11 +1,66 @@
+// import path from 'path';
+// import express from 'express';
+// import multer from 'multer';
+
+// const router = express.Router();
+
+// const storage = multer.diskStorage({
+//   destination(req, file, cb) {
+//     cb(null, 'uploads/');
+//   },
+//   filename(req, file, cb) {
+//     cb(
+//       null,
+//       `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+//     );
+//   },
+// });
+
+// function fileFilter(req, file, cb) {
+//   const filetypes = /jpe?g|png|webp/;
+//   const mimetypes = /image\/jpe?g|image\/png|image\/webp/;
+
+//   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+//   const mimetype = mimetypes.test(file.mimetype);
+
+//   if (extname && mimetype) {
+//     cb(null, true);
+//   } else {
+//     cb(new Error('Images only!'), false);
+//   }
+// }
+
+// const upload = multer({ storage, fileFilter });
+// const uploadSingleImage = upload.single('image');
+
+// router.post('/', (req, res) => {
+//   uploadSingleImage(req, res, function (err) {
+//     if (err) {
+//       return res.status(400).send({ message: err.message });
+//     }
+
+//     res.status(200).send({
+//       message: 'Image uploaded successfully',
+//       image: `/${req.file.path}`,
+//     });
+//   });
+// });
+
+// export default router;
+
+
 import path from 'path';
 import express from 'express';
 import multer from 'multer';
-
 const router = express.Router();
+
+// If production, use Render server's data folder, else use local uploads folder
+const uploadFolder =
+  process.env.NODE_ENV === 'production' ? '/var/data/uploads/' : 'uploads/';
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
+    cb(null, uploadFolder);
     cb(null, 'uploads/');
   },
   filename(req, file, cb) {
@@ -15,35 +70,23 @@ const storage = multer.diskStorage({
     );
   },
 });
-
-function fileFilter(req, file, cb) {
-  const filetypes = /jpe?g|png|webp/;
-  const mimetypes = /image\/jpe?g|image\/png|image\/webp/;
-
+function checkFileType(file, cb) {
+  const filetypes = /jpg|jpeg|png/;
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = mimetypes.test(file.mimetype);
-
+  const mimetype = filetypes.test(file.mimetype);
   if (extname && mimetype) {
-    cb(null, true);
+    return cb(null, true);
   } else {
-    cb(new Error('Images only!'), false);
+    cb({ message: 'Images only!' });
   }
 }
-
-const upload = multer({ storage, fileFilter });
-const uploadSingleImage = upload.single('image');
-
-router.post('/', (req, res) => {
-  uploadSingleImage(req, res, function (err) {
-    if (err) {
-      return res.status(400).send({ message: err.message });
-    }
-
-    res.status(200).send({
-      message: 'Image uploaded successfully',
-      image: `/${req.file.path}`,
-    });
+const upload = multer({
+  storage,
+});
+router.post('/', upload.single('image'), (req, res) => {
+  res.send({
+    message: 'Image uploaded successfully',
+    image: `/${req.file.path}`,
   });
 });
-
 export default router;
